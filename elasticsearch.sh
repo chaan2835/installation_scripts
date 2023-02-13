f555b946c2d68bb3942570f6ca0e860a1618dba8
chaan2835 <92902854+chaan2835@users.noreply.github.com>
2023-02-13 17:23:30 +0530

    elasticsearch script file added

--- elasticsearch.sh
+++ elasticsearch.sh
@@ -4,4 +4,4 @@ sudo apt install tar -y
 sudo tar -zxvf elasticsearch-7.15.0-linux-x86_64.tar.gz
 sudo mv elasticsearch-7.15.0 elastic-search 
 sudo apt update
-sudo apt install openjdk-8-jdk openjdk-8-jre
+sudo apt install openjdk-8-jdk openjdk-8-jre -y
